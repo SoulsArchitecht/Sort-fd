6940b16afaacdcdb9e852933fc45b9fbdec78a74
Serge <sshibkojavadev@gmail.com>
2023-08-17 23:52:47 +0800

    added) MergeSorter and FileHandler and Main edit) pom.xml

--- Sort-fd/src/main/java/Main.java
+++ Sort-fd/src/main/java/Main.java
@@ -1,5 +1,26 @@
+import argsToOptionParser.ArgsParser;
+import argsToOptionParser.ArgsParserImpl;
+import argsToOptionParser.SortParameters;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sortFileData.FileMergeSorter;
+
 public class Main {
+
+    private final static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArgsParser argsParser = ArgsParserImpl.getInstance();
+
+        try {
+            SortParameters sortParameters = argsParser.parseInputArgs(args);
+            FileMergeSorter fileMergeSorter = new FileMergeSorter();
+            fileMergeSorter.sort(sortParameters);
+            System.out.println("Operations done. You can see result at file: " + sortParameters.getOutputFileName());
+        } catch (IllegalArgumentException | ParseException e) {
+            logger.error(e.getMessage());
+            System.exit(1);
+        }
     }
 }
